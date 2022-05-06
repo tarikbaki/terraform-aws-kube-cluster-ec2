7329030eaf5b1d66bc3945d0acb20687f273e852
Tarik Baki <tarikbaki@hotmail.com>
2022-05-06 18:32:22 +0300

    commit2

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "master-ip" {
-    value = aws_instance.master.public_ip
+  value = aws_instance.master.public_ip
 }
 output "worker-private-ip" {
-    value = {
-        for worker in aws_instance.worker : worker.tags["Name"] => worker.private_ip
-    }
+  value = {
+    for worker in aws_instance.worker : worker.tags["Name"] => worker.private_ip
+  }
 }
 
 output "worker-public-ip" {
-    value = {
-        for worker in aws_instance.worker : worker.tags["Name"] => worker.public_ip
-    }
+  value = {
+    for worker in aws_instance.worker : worker.tags["Name"] => worker.public_ip
+  }
 }
--- provider.tf
+++ provider.tf
@@ -1,23 +1,23 @@
 terraform {
-    required_version = "~>1.0"
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~>3.0"
-        }
-        http = {
-            source = "hashicorp/http"
-            version = "2.1.0"
-        }
-        random = {
-            source = "hashicorp/random"
-            version = "3.1.0"
-        }
+  required_version = "~>1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
     }
+    http = {
+      source  = "hashicorp/http"
+      version = "2.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
 }
 provider "aws" {
-    region = "eu-west-2"
-   
+  region = "eu-west-2"
+
 }
 provider "http" {}
 provider "random" {}
