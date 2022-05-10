7d2c50475cc43160dc327d96da5b1078b4fe135f
Tarik Baki <tarikbaki@hotmail.com>
2022-05-10 21:06:52 +0300

    docker file & yml edit

--- provider.tf
+++ provider.tf
@@ -1,23 +1,23 @@
 terraform {
-  required_version = "~>1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~>3.0"
+    required_version = "~>1.0"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~>3.0"
+        }
+        http = {
+            source = "hashicorp/http"
+            version = "2.1.0"
+        }
+        random = {
+            source = "hashicorp/random"
+            version = "3.1.0"
+        }
     }
-    http = {
-      source  = "hashicorp/http"
-      version = "2.1.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.1.0"
-    }
-  }
 }
 provider "aws" {
-  region = "eu-west-2"
-
+    region = "eu-central-1"
+    profile = "tarikbaki3"
 }
 provider "http" {}
 provider "random" {}
--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 variable "worker-count" {
-  default = 1
+    default = 1
 }
 variable "vpc_cidr_range" {
-  default = "10.0.0.0/16"
+    default = "10.0.0.0/16"
 }
-variable "vpc_private_subnets" {
-  default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+variable "vpc_private_subnets"{
+    default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
 variable "vpc_public_subnets" {
-  default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+    default = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 }
 variable "key_name" {
-  default = "id_rsa"
+    default = ""
 }
